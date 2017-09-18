f7b8346e38c5aca91828b939cc7b073c2ca94524
Francois Mirallès <miralles.francois@ireq.ca>
2017-09-18 13:42:30 -0400

    Grab images from video instead of camera. Apply filtering in a loop.

--- PtGreyCapture.cpp
+++ PtGreyCapture.cpp
@@ -13,6 +13,8 @@
 #include "PGRDevice.h"
 
 #include <opencv2/cudafilters.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
 
 using namespace std;
 using namespace uav::plvision;
@@ -24,29 +26,37 @@ int main(int argc, char** argv)
     cv::Mat g0(5,1,CV_32F);
     cv::Mat g1(5,1,CV_32F);
     
-    cv::Mat img = cv::Mat::zeros(1000,1000,CV_32F);
-    cv::cuda::GpuMat d_inputImgF;
-    d_inputImgF.upload(img);
+    cv::Mat img, grey;
+    cv::cuda::GpuMat d_img, d_inputImgF;
+    
+    cv::VideoCapture cap("myvideo.avi");
     
     cv::cuda::GpuMat md_dfdg0;
     
-    // Comment the next 2 lines to get highr frame rate
-    //Ptr<Filter> f_g0_g1 = cv::cuda::createSeparableLinearFilter(CV_32F, CV_32F, g0, g1);
-    //f_g0_g1->apply(d_inputImgF, md_dfdg0);
-    //
-
-	PGRDevice device;
-	   
-    device.configure();
+    Ptr<Filter> f_g0_g1 = cv::cuda::createSeparableLinearFilter(CV_32F, CV_32F, g0, g1);
     
-    device.start();
+    std::chrono::time_point<std::chrono::steady_clock> mPrevCapTime;
     
-  	cout << "Done! Press Enter to exit..." << endl;
-	cin.ignore();
-
-	device.stop();
-	
-	device.cleanup();
+    int c = -1;
+    while(c==-1)
+    {
+        cap.read(img);
+        cv::cvtColor(img, grey, CV_BGR2GRAY);
+        
+        d_img.upload(grey);
+        
+        d_img.convertTo(d_inputImgF, CV_32F, 1.0);
+        
+        f_g0_g1->apply(d_inputImgF, md_dfdg0);
+        
+        auto t1 = std::chrono::steady_clock::now();
+    
+        std::cout << static_cast<int>(std::chrono::duration_cast<std::chrono::milliseconds>(t1 - mPrevCapTime).count()) << std::endl;
+    
+        mPrevCapTime = t1;
+    
+        c = cv::waitKey(5);
+    }
 
 	return 0;
 };
